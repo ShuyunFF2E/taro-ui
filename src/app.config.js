68b26e5733a041cb8b6ae4d37a3d3f8e04d25fd0
zhangruixue012 <18729255769@163.com>
2020-09-07 17:24:19 +0800

    :sparkles: [tag] 开发tag组件 & 给button组件添加hover样式

--- src/app.config.js
+++ src/app.config.js
@@ -2,7 +2,15 @@ export default {
   pages: [
     'pages/index/index',
     'pages/input/index',
-    'pages/button/index'
+    'pages/button/index',
+    // 'pages/checkbox/index',
+    'pages/tag/index',
+    // 'pages/pagination/index',
+    // 'pages/select/index',
+    // 'pages/tab/index',
+    // 'pages/table/index',
+    // 'pages/message/index',
+    // 'pages/modal/index',
   ],
   window: {
     backgroundTextStyle: 'light',

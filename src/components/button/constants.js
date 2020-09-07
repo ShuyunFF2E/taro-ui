27ca9ff8008d91ea9001bd24ed859f994d8295f0
zhangruixue012 <18729255769@163.com>
2020-09-07 10:14:54 +0800

    :sparkles:[button] 开发button组件

---
+++ src/components/button/constants.js
@@ -0,0 +1,13 @@
+export const TYPES = {
+  primary: 'primary',
+  link: 'link',
+  normal: 'normal'
+}
+
+export const SIZE = {
+  small: 'small',
+  default: 'default',
+  large: 'large'
+}
+
+export default {};
--- src/pages/button/index.jsx
+++ src/pages/button/index.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-
 import { View } from '@tarojs/components';
+import CButton from '../../components/button';
 
 @observer
 class Index extends Component {
 
   render() {
     return (
-      <View>button的demo</View>
+      <View>
+        <CButton style={{ margin:'10px' }}>重置</CButton>
+
+        <CButton type="primary" style={{ margin:'10px' }}>搜索</CButton>
+
+        <CButton type="link" style={{ margin:'10px' }}>链接</CButton>
+
+        <CButton type="primary" size="large" style={{ margin:'10px' }}>确定</CButton>
+
+        <CButton size="large" style={{ margin:'10px' }}>取消</CButton>
+
+        <CButton size="large" style={{ margin:'10px' }}>新增</CButton>
+      </View>
     );
   }
 }

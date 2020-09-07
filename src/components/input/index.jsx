4f9419919a064615121233c79f8a7c6d52d040e7
SitongHui <492150523@qq.com>
2020-09-07 14:04:07 +0800

    input组件

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
-import { View } from '@tarojs/components';
+import { View, Input, Text } from '@tarojs/components';
+import clz from 'classnames';
+
 import { prefixCls } from '../const';
 import './index.less';
 
 const classSelector = `${prefixCls}-input`;
 
 @observer
-class StInput extends Component {
+class CInput extends Component {
+  static propTypes = {
+    width: PropTypes.number,
+    maxLength: PropTypes.number,
+    disabled: PropTypes.bool,
+    value: PropTypes.string,
+    onInput: PropTypes.func,
+  };
+
+  static defaultProps = {
+    width: 150,
+    maxLength: null,
+    disabled: false,
+    value: '',
+    onInput: () => {},
+  };
+
+  onInputValue = e => {
+    this.props.onInput(e);
+  };
+
   render() {
-    return <View className={classSelector}>我是input组件</View>;
+    const { label, placeholder, width, maxLength, disabled, value, onInput } = this.props;
+    const _width = width + 'px';
+
+    return (
+      <View>
+        {label && <Text className={`${classSelector}-label`}>{label}</Text>}
+        <Input
+          value={value}
+          placeholder={placeholder}
+          maxlength={maxLength}
+          disabled={disabled}
+          className={clz(disabled ? `${classSelector}-disabled` : '', `${classSelector}-style`)}
+          style={{ width: _width }}
+          onInput={this.onInputValue}
+        />
+      </View>
+    );
   }
 }
 
-export default StInput;
+export default CInput;
--- src/pages/input/index.jsx
+++ src/pages/input/index.jsx
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import { Text, View } from '@tarojs/components';
 import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 
-import StInput from '../../components/input'
+import CInput from '../../components/input';
 
 @observer
 class InputDemo extends Component {
+  @observable
+  basicInputValue = '';
+
+  @observable
+  disabledInputValue = '';
+
+  onInput = e => {
+    this.inputValue = e.target.value;
+  };
+
+  onDisabledInput = e => {
+    this.disabledInputValue = e.target.value;
+  };
 
   render() {
     return (
-      <StInput />
+      <View>
+        <Text>Input 输入框</Text>
+        <CInput label="标题：" value={this.basicInputValue} placeholder="请输入标题" width={180} maxLength={20} onInput={this.onInput} />
+        <CInput label="disabled：" value={this.disabledInputValue} placeholder="disabled" disabled={true} onInput={this.onDisabledInput} />
+      </View>
     );
   }
 }
